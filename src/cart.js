61683b8ee0ec7a2f261958303b801b066db569da
Ben Christiansen <bwc1@iastate.edu>
2024-04-04 17:09:47 -0500

    now displays products, added bootstrap

--- src/cart.js
+++ src/cart.js
@@ -1,80 +1,164 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import items from "./products.json";
+import "bootstrap/dist/css/bootstrap.css";
 
 var order = {
-    name: "",
-    email: "",
-    card: "",
-    address: "",
-    address2: "",
-    city: "",
-    state: "",
-    zip: ""
+  name: "",
+  email: "",
+  card: "",
+  address: "",
+  address2: "",
+  city: "",
+  state: "",
+  zip: "",
 };
 
-function validateOrder(order) {
-    let bool = true;
-    let email = document.getElementById("email");
-    let card = document.getElementById("card");
-    let address = document.getElementById("address");
-    let address2 = document.getElementById("address2");
-    let city = document.getElementById("city");
-    let state = document.getElementById("state");
-    let zip = document.getElementById("zip");
-    const cardSum = document.querySelector(".card");
-    const sList = document.querySelector(".card > ul");
-    const alerter = document.getElementById("alerterHolder");
+const Shop = () => {
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
 
-    if (order.name === "" && order.email === "" && order.card === "" && order.address === "" && order.city === "" && order.state === "" && order.zip === "") {
-        alert("Please enter a name.");
-        bool = false;
-    }
-    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email.value)) {
-        alert("Please enter a valid email.");
-        bool = false;
-    }
-    if (!/^\d{16}$/.test(card.value)) {
-        alert("Please enter a valid card number.");
-        bool = false;
-    }
-    if (!/^[a-zA-Z0-9\s,'-]*$/.test(address.value)) {
-        alert("Please enter a valid address.");
-        bool = false;
-    }
-    if (!/^[a-zA-Z\s-]+$/.test(city.value)) {
-        alert("Please enter a valid city.");
-        bool = false;
-    }
-    if (!/^[a-zA-Z\s-]+$/.test(state.value)) {
-        alert("Please enter a valid state.");
-        bool = false;
-    }
-    if (!/^\d{5}$/.test(zip.value)) {
-        alert("Please enter a valid zip code.");
-        bool = false;
-    }
+  //   useEffect(() => {
+  //     total();
+  //   }, [cart]);
 
+  const listItems = items.map((el) => (
+    // PRODUCT
+    <div class="row border-top border-bottom" key={el.id}>
+      <div class="row main align-items-center">
+        <div class="col-2">
+          <img class="img-fluid" src={el.image} />
+        </div>
+        <div class="col">
+          <div class="row text-muted">{el.title}</div>
+          <div class="row">{el.category}</div>
+        </div>
+        <div class="col">
+          <button
+            type="button"
+            variant="light"
+            // onClick={() => removeFromCart(el)}
+          >
+            {" "}
+            -{" "}
+          </button>{" "}
+          <button
+            type="button"
+            variant="light"
+            //   onClick={() => addToCart(el)}
+          >
+            {" "}
+            +{" "}
+          </button>
+        </div>
+        <div class="col">
+          ${el.price} <span class="close">&#10005;</span>
+          {/* {howManyofThis(el.id)} */}
+        </div>
+      </div>
+    </div>
+  ));
 
-    if (bool) {
-        form.classList.add("collapse");
-        backButton.classList.add("collapse");
+  // function validateOrder(order) {
+  //   let bool = true;
+  //   let email = document.getElementById("email");
+  //   let card = document.getElementById("card");
+  //   let address = document.getElementById("address");
+  //   let address2 = document.getElementById("address2");
+  //   let city = document.getElementById("city");
+  //   let state = document.getElementById("state");
+  //   let zip = document.getElementById("zip");
+  //   const cardSum = document.querySelector(".card");
+  //   const sList = document.querySelector(".card > ul");
+  //   const alerter = document.getElementById("alerterHolder");
 
-        for (var [key, value] of Object.entries(order)) {
-            console.log(key, value);
+  //   if (
+  //     order.name === "" &&
+  //     order.email === "" &&
+  //     order.card === "" &&
+  //     order.address === "" &&
+  //     order.city === "" &&
+  //     order.state === "" &&
+  //     order.zip === ""
+  //   ) {
+  //     alert("Please enter a name.");
+  //     bool = false;
+  //   }
+  //   if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email.value)) {
+  //     alert("Please enter a valid email.");
+  //     bool = false;
+  //   }
+  //   if (!/^\d{16}$/.test(card.value)) {
+  //     alert("Please enter a valid card number.");
+  //     bool = false;
+  //   }
+  //   if (!/^[a-zA-Z0-9\s,'-]*$/.test(address.value)) {
+  //     alert("Please enter a valid address.");
+  //     bool = false;
+  //   }
+  //   if (!/^[a-zA-Z\s-]+$/.test(city.value)) {
+  //     alert("Please enter a valid city.");
+  //     bool = false;
+  //   }
+  //   if (!/^[a-zA-Z\s-]+$/.test(state.value)) {
+  //     alert("Please enter a valid state.");
+  //     bool = false;
+  //   }
+  //   if (!/^\d{5}$/.test(zip.value)) {
+  //     alert("Please enter a valid zip code.");
+  //     bool = false;
+  //   }
 
-            if (key === "card") {
-                value = value.replace(/\d(?=\d{4})/g, "*");
-            }
-        }
-        sList.innerHTML = `
-            <li>${key}</li>
-            <li>${value}</li>
-        `;
-        cardSum.classList.remove("collapse");
-        alerter.innerHTML = "";
-        alert(
-            '<li>You have completed your order!</li>',
-            "success"
-        );
-    }
-    return bool;
-}
+  //   if (bool) {
+  //     form.classList.add("collapse");
+  //     backButton.classList.add("collapse");
+
+  //     for (var [key, value] of Object.entries(order)) {
+  //       console.log(key, value);
+
+  //       if (key === "card") {
+  //         value = value.replace(/\d(?=\d{4})/g, "*");
+  //       }
+  //     }
+  //     sList.innerHTML = `
+  //             <li>${key}</li>
+  //             <li>${value}</li>
+  //         `;
+  //     cardSum.classList.remove("collapse");
+  //     alerter.innerHTML = "";
+  //     alert("<li>You have completed your order!</li>", "success");
+  //   }
+  //   return bool;
+  // }
+
+  return (
+    <div>
+      STORE
+      <div class="card">
+        <div class="row">
+          {/* HERE, IT IS THE SHOPING CART */}
+          <div class="col-md-8 cart">
+            <div class="title">
+              <div class="row">
+                <div class="col">
+                  <h4>{/* <b>Shopping Cart</b> */}</h4>
+                </div>
+                <div class="col align-self-center text-right text-muted">
+                  {/* Products selected {cart.length} */}
+                </div>
+              </div>
+            </div>
+            <div>{listItems}</div>
+          </div>
+          <div class="float-end">
+            <p class="mb-0 me-5 d-flex align-items-center">
+              <span class="small text-muted me-2">Order total:</span>
+              <span class="lead fw-normal">${/* {cartTotal} */}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
